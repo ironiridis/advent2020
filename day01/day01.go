527909eb82fed43ee476d779b6c19749fca325e7
Chris Harrington <ironiridis@gmail.com>
2020-12-25 12:48:12 -0600

    add part 2 stub

--- day01/day01.go
+++ day01/day01.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"strconv"
 
 	"github.com/ironiridis/advent2020/scando"
 )
 
-func findSumTo2020(in chan string) (string, error) {
+func findTwoSumTo2020(in chan string) (string, error) {
 	m := make(map[int]bool)
 	for s := range in {
 		val, err := strconv.Atoi(s)
@@ -22,12 +23,40 @@ func findSumTo2020(in chan string) (string, error) {
 	return "", fmt.Errorf("no solution found")
 }
 
-func main() {
+func findThreeSumTo2020(in chan string) (string, error) {
+	return "", fmt.Errorf("unimplemented 😬")
+}
+
+func part1() {
 	fmt.Println("Day 1, part 1 - input expense report values")
-	ans, err := findSumTo2020(scando.Stdin())
+	ans, err := findTwoSumTo2020(scando.Stdin())
 	if err != nil {
 		fmt.Printf("Cannot determine answer: %v\n", err)
 		return
 	}
 	fmt.Printf("Part 1 Answer: %q\n", ans)
+}
+
+func part2() {
+	fmt.Println("Day 1, part 2 - input expense report values again")
+	ans, err := findThreeSumTo2020(scando.Stdin())
+	if err != nil {
+		fmt.Printf("Cannot determine answer: %v\n", err)
+		return
+	}
+	fmt.Printf("Part 2 Answer: %q\n", ans)
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "Which Part of the challenge to run")
+	flag.Parse()
+	switch part {
+	case 1:
+		part1()
+	case 2:
+		part2()
+	default:
+		panic(fmt.Errorf("Cannot run part %d", part))
+	}
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -6,12 +6,20 @@ import (
 	"github.com/ironiridis/advent2020/scando"
 )
 
-func TestProblemExample(t *testing.T) {
-	scando.Strap(findSumTo2020, `1721
+func TestPart1Example(t *testing.T) {
+	scando.Strap(findTwoSumTo2020, `1721
 		979
 		366
 		299
 		675
 		1456`, "514579", t)
+}
 
+func TestPart2Example(t *testing.T) {
+	scando.Strap(findThreeSumTo2020, `1721
+		979
+		366
+		299
+		675
+		1456`, "241861950", t)
 }
