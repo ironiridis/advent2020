d997b8dd9c2da91de47348a4c6d614fff926c808
Chris Harrington <ironiridis@gmail.com>
2020-12-26 20:01:56 -0600

    add field value validation and complete part 2

--- day04/day04.go
+++ day04/day04.go
@@ -8,6 +8,11 @@ import (
 	"github.com/ironiridis/advent2020/scando"
 )
 
+var validateHeight = regexp.MustCompile(`^([0-9]{2,3})(in|cm)$`)
+var validateHair = regexp.MustCompile(`^#[0-9a-f]{6}$`)
+var validateEyes = regexp.MustCompile(`^(amb|blu|brn|gry|grn|hzl|oth)$`)
+var validatePID = regexp.MustCompile(`^[0-9]{9}$`)
+
 type passport map[string]string
 
 func readPassport(in chan string) (p passport, done bool, err error) {
@@ -38,7 +43,7 @@ func readPassport(in chan string) (p passport, done bool, err error) {
 	}
 }
 
-func (p passport) valid() bool {
+func (p passport) validFieldsPresent() bool {
 	if p == nil {
 		return false
 	}
@@ -67,7 +72,73 @@ func (p passport) valid() bool {
 	return true
 }
 
+func (p passport) validValues() bool {
+	if byr, err := strconv.Atoi(p["byr"]); err != nil || byr < 1920 || byr > 2002 {
+		return false
+	}
+
+	if iyr, err := strconv.Atoi(p["iyr"]); err != nil || iyr < 2010 || iyr > 2020 {
+		return false
+	}
+
+	if eyr, err := strconv.Atoi(p["eyr"]); err != nil || eyr < 2020 || eyr > 2030 {
+		return false
+	}
+
+	hgt := validateHeight.FindStringSubmatch(p["hgt"])
+	if hgt == nil {
+		return false
+	}
+	hgtn, err := strconv.Atoi(hgt[1])
+	if err != nil {
+		return false
+	}
+	switch hgt[2] {
+	case "cm":
+		if hgtn < 150 || hgtn > 193 {
+			return false
+		}
+	case "in":
+		if hgtn < 59 || hgtn > 76 {
+			return false
+		}
+	default: /* unrecognized unit, though should be covered by regex */
+		return false
+	}
+	if !validateHair.MatchString(p["hcl"]) {
+		return false
+	}
+	if !validateEyes.MatchString(p["ecl"]) {
+		return false
+	}
+	if !validatePID.MatchString(p["pid"]) {
+		return false
+	}
+	return true
+}
+
+func (p passport) valid() bool {
+	return p.validFieldsPresent() && p.validValues()
+}
+
 func countValidPassportsPart1(in chan string) (string, error) {
+	var valid int
+	for {
+		p, done, err := readPassport(in)
+		if err != nil {
+			return "", err
+		}
+		if p.validFieldsPresent() {
+			valid++
+		}
+		if done {
+			break
+		}
+	}
+	return strconv.Itoa(valid), nil
+}
+
+func countValidPassportsPart2(in chan string) (string, error) {
 	var valid int
 	for {
 		p, done, err := readPassport(in)
@@ -84,19 +155,15 @@ func countValidPassportsPart1(in chan string) (string, error) {
 	return strconv.Itoa(valid), nil
 }
 
-func countValidPassportsPart2(in chan string) (string, error) {
-	return "", fmt.Errorf("unimplemented")
-}
-
 func main() {
-	fmt.Println("Day 3, part 1 - valid passport count")
+	fmt.Println("Day 3, part 1 - valid passport count, just checking field presence")
 	ans, err := countValidPassportsPart1(scando.Input())
 	if err != nil {
 		fmt.Printf("Cannot determine answer: %v\n", err)
 		return
 	}
 	fmt.Printf("Part 1 Answer: %q\n", ans)
-	fmt.Println("Day 3, part 2 ...")
+	fmt.Println("Day 3, part 2 - valid passport count, with value validation")
 	ans, err = countValidPassportsPart2(scando.Input())
 	if err != nil {
 		fmt.Printf("Cannot determine answer: %v\n", err)
