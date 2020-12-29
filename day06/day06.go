0f019e53177c9643f285f29029504385e01b4c7b
Chris Harrington <ironiridis@gmail.com>
2020-12-28 20:27:53 -0600

    stub day 6

---
+++ day06/day06.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ironiridis/advent2020/scando"
+)
+
+func part1func(in chan string) (string, error) {
+	return "", fmt.Errorf("unimplemented")
+}
+
+func part2func(in chan string) (string, error) {
+	return "", fmt.Errorf("unimplemented")
+}
+
+func main() {
+	fmt.Println("Day 6, part 1 - summary")
+	ans, err := part1func(scando.Input())
+	if err != nil {
+		fmt.Printf("Cannot determine answer: %v\n", err)
+		return
+	}
+	fmt.Printf("Part 1 Answer: %q\n", ans)
+	fmt.Println("Day 6, part 2 - summary")
+	ans, err = part2func(scando.Input())
+	if err != nil {
+		fmt.Printf("Cannot determine answer: %v\n", err)
+		return
+	}
+	fmt.Printf("Part 2 Answer: %q\n", ans)
+}
