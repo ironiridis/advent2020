98f8e023b387fe6ce019156c62ed0b98b443d296
Chris Harrington <ironiridis@gmail.com>
2020-12-31 18:28:35 -0600

    part 2 test

--- day09/day09_test.go
+++ day09/day09_test.go
@@ -38,5 +38,24 @@ func TestPart1Example(t *testing.T) {
 }
 
 func TestPart2Example(t *testing.T) {
-	t.Fail()
+	scando.Strap(part2funcT, `35
+	20
+	15
+	25
+	47
+	40
+	62
+	55
+	65
+	95
+	102
+	117
+	150
+	182
+	127
+	219
+	299
+	277
+	309
+	576`, "62", t)
 }
