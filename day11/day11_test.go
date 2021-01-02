7a0218a9c7fdc2a2af81d15b4f50bbfe8f0c77d5
Chris Harrington <ironiridis@gmail.com>
2021-01-01 19:41:38 -0600

    stub part 1

---
+++ day11/day11_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ironiridis/advent2020/scando"
+)
+
+func TestPart1Example(t *testing.T) {
+	scando.Strap(part1func,
+		`L.LL.LL.LL
+		 LLLLLLL.LL
+		 L.L.L..L..
+		 LLLL.LL.LL
+		 L.LL.LL.LL
+		 L.LLLLL.LL
+		 ..L.L.....
+		 LLLLLLLLLL
+		 L.LLLLLL.L
+		 L.LLLLL.LL`, "37", t)
+}
+
+func TestPart2Example(t *testing.T) {
+	t.Fail()
+}
